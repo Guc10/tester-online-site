c773bd6a194480ef076823a2574acdf23ea67ab4
Gustaw Grześkowiak <gustaw.grzeskowiak@gmail.com>
2025-05-17 19:26:50 +0200

    test5

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (_, res) => res.send('test3'));
+app.get('/', (_, res) => res.send('test5'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
